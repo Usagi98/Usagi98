ad0457a0477605ba4cfd018ee68c04fc872ae8b2
Usagi98 <73405913+Usagi98@users.noreply.github.com>
2020-12-04 16:30:18 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$ (document) .ready(function() {
+    $('.switch') .click (function() {
+        $('.menu') .toggleClass ('visible');
+    });
+});
+$('.przyklad').hide();
+$('#klik').click(function(){
+    $('.przyklad').slideDown(1000);
+});
+
+$(document) .ready(function(){
+  $(".pass") .focus(function(){
+    $(this) .css("background-color","#CCFFFF");
+  });
+  $(".pass") .blur(function(){
+    $(this) .css("background-color", "white");
+  });
+});
+$ (document) .ready(function() {
+    $('.switch') .click (function() {
+     $('.line') .css("text-decoration", "overline", "#5D2275");
+    });
+});
+document.getElementById("help") .innerHTML=("Książki, z których korzystam"); 
+document.getElementById("help").style.textTransform ="uppercase";
+
+
+
